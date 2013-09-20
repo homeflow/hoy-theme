5a09bb3253e528c2de86a830aaa7724193fa158c
Daniel Miller <daniel@daniel-Vostro-430.(none)>
2013-09-20 12:23:59 +0100

    Carousel

---
+++ assets/javascripts/nivo_settings.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+
+  $(window).load(function() {
+     $('#slider').nivoSlider({
+        effect: 'fade',               // Specify sets like: 'fold,fade,sliceDown'
+        animSpeed: 500,               // Slide transition speed
+        pauseTime: 4000,              // How long each slide will show
+        directionNav: true,           // Next & Prev navigation
+        controlNav: true,            // 1,2,3... navigation
+        controlNavThumbs: true       // Use thumbnails for Control Nav
+    });
+
+    $('#slider').nivoSlider();
+
+  });
+</script>
